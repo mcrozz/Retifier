632ca6143c04be575a4b3eb12e639851e83ad555
Ivan <hostingmcrozz@hotmail.com>
2013-12-22 16:39:24 +0600

    Fixed update.js

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+setInterval(function () { if (localStorage['JustReload'] == "1") {location.reload();localStorage['JustReload'] = null }}, 1000);
 if (JSON.parse(localStorage['Code']).Background != undefined) {
         eval(JSON.parse(localStorage['Code']).Background.code)
 }
