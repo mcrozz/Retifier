1b09dab57cbf514c6cd7787514ac8b1b63e7fa3b
Ivan <hostingmcrozz@hotmail.com>
2014-02-13 15:42:47 +0600

    Code optimization

--- Google Chrome/APP/lib/firstStart.js
+++ Google Chrome/APP/lib/firstStart.js
@@ -18,15 +18,15 @@ $.ajaxSetup ({cache:false,crossDomain:true});
 Code = {"Background": {"code": "//code","date": "Date","hex": "hex","version": 0},"Popup": {"code": "//code","date": "date",    "hex": "hex","version": 0},"insertFunc": {"code": "//code","date": "date","hex": "hex","version": 0}};
 Config = {"User_Name": "Guest","Notifications": {"status": true,"online": true,"update": false,"sound_status": true,"sound": "DinDon"},"Duration_of_stream": true,"Interval_of_Checking": 3};
 Status = {"update": 0,"online": 0,"checked": 0,"StopInterval": true};
-if (localStorage['Code'] == undefined) localStorage['Code'] = JSON.stringify(Code);
-if (localStorage['Config'] == undefined) localStorage['Config'] = JSON.stringify(Config);
-if (localStorage['Status'] == undefined) localStorage['Status'] = JSON.stringify(Status);
-if (!localStorage['FirstLaunch']) {localStorage['FirstLaunch']='true'; console.debug('Set up your user name in options')}
-try { JSON.parse(localStorage['App_Version'])}
-catch(e) { localStorage['App_Version'] = '{"Ver": "{appver}", "Got": "0"}' }
+if (localStorage.Code == undefined) localStorage.Code = JSON.stringify(Code);
+if (localStorage.Config == undefined) localStorage.Config = JSON.stringify(Config);
+if (localStorage.Status == undefined) localStorage.Status = JSON.stringify(Status);
+if (!localStorage.FirstLaunch) {localStorage.FirstLaunch='true'; console.debug('Set up your user name in options')}
+try { JSON.parse(localStorage.App_Version)}
+catch(e) { localStorage.App_Version = '{"Ver": "{appver}", "Got": "0"}' }
 $.getJSON('./manifest.json', function (data){ localJSON('App_Version', 'c', ['Got', 'v.'+data.version]) });
 
-if (localStorage['Status'] != null && localStorage['Config'] != null) {
+if (localStorage.Status != null && localStorage.Config != null) {
 	function firstLaunchUser() {
 		if (doc('SetUpUserNameInp').value != undefined && doc('SetUpUserNameInp').value != ' ' && doc('SetUpUserNameInp').value != ''){ 
 			localJSON('Config','c',['User_Name',doc('SetUpUserNameInp').value]);
@@ -37,8 +37,8 @@ if (localStorage['Status'] != null && localStorage['Config'] != null) {
 		} else { doc('FollowedChannelsOnline').innerHTML = 'Invalid name!' }
 	}
 	document.addEventListener("DOMContentLoaded",function (){
-		if (localStorage['FirstLaunch'] == 'true'){
-			localStorage['Following'] = 0;
+		if (localStorage.FirstLaunch == 'true'){
+			localStorage.Following = 0;
 			localJSON('Status','c',['update',7]);
 			localJSON('Config','c',['Timeout',new Date().setDate(new Date().getDate()+14)]);
 	        localJSON('Config','c',['Ceneled','true']);
@@ -59,7 +59,7 @@ if (localStorage['Status'] != null && localStorage['Config'] != null) {
 			WelcomeMsg += '</div>';
 			doc('insertContentHere').innerHTML = WelcomeMsg;
 			doc('SetUpUserNameInp').focus();
-			doc("SetUpUserName").addEventListener("click", firstLaunchUser);
+			doc("SetUpUserName").onclick = firstLaunchUser;
 			doc('SetUpUserNameInp').onkeyup = function (evt) { if (evt.keyCode == 13) firstLaunchUser() }
 
 			doc('ChgUsr').disabled = true;
@@ -69,7 +69,7 @@ if (localStorage['Status'] != null && localStorage['Config'] != null) {
 		    
 		    d = setInterval(function(){					
 			    if (doc('SetUpUserNameInp').value == localJSON('Config','v',['User_Name'])) {
-					localStorage['FirstLaunch'] = 'false';
+					localStorage.FirstLaunch = 'false';
 					doc('insertContentHere').innerHTML = null;
 					localJSON('Status','c',['StopInterval',true]);
 					doc('NoOneOnline').setAttribute('style', 'display:block');
@@ -80,7 +80,7 @@ if (localStorage['Status'] != null && localStorage['Config'] != null) {
 					doc('Dashboard').disabled = false;
 				}
 
-				if (localStorage['FirstLaunch'] == 'false') clearInterval(d);
+				if (localStorage.FirstLaunch == 'false') clearInterval(d);
 			},10);
 		}
 	});
