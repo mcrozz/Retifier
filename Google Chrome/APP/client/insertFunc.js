dd736289965c6b3ec10c1fba15cda817dff71393
Ivan <hostingmcrozz@hotmail.com>
2014-02-14 19:18:18 +0600

    Delete useless files

--- Google Chrome/APP/client/insertFunc.js
+++
@@ -1,3 +0,0 @@
-if (localStorage['Code'] != undefined) {
-        eval(JSON.parse(localStorage['Code']).insertFunc.code)
-}
--- Google Chrome/APP/client/popup.js
+++
@@ -1,4 +0,0 @@
-if (localStorage['Status'] != undefined && localStorage['Config'] != undefined) {
-        _gaq.push(['_trackPageview']);
-        eval(JSON.parse(localStorage['Code']).Popup.code)
-}
