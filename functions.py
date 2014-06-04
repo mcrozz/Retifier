a05bc16dba92e619bdae4db2cd4ad43e2231629c
Ivan <hostingmcrozz@hotmail.com>
2014-06-04 15:37:50 +0600

    Code optimization
    Deleted colon. function pj now accept array as parameter

--- functions.py
+++ functions.py
@@ -18,22 +18,26 @@ tld = "====================================================="
 
 def rp(w, j, o):
 	with open(o,'r') as f:
-		newlines = [];
+		newlines = []
 		for line in f.readlines():
-			newlines.append(line.replace(w, j));
+			newlines.append(line.replace(w, j))
 	with open(o, 'w') as f:
-		for line in newlines: f.write(line);
-	f.close();
+		for line in newlines: f.write(line)
+	f.close()
 
-def pj(f, s):
-	return os.path.join(f, s);
+def pj(g):
+	h = os.path.join(g[0], g[1])
+	for f in g:
+		if f != g[0] and f != g[1]:
+			h = os.path.join(h, f)
+	return h
 
 def sc(c):
 	with open('config.json', 'w') as o:
-		json.dump(c, o);
+		json.dump(c, o)
 
 def cf():
-	return json.load(open('config.json'));
+	return json.load(open('config.json'))
 
 def rf(f):
 	with open(f, 'r') as o:
@@ -47,7 +51,7 @@ def int(f):
 		print "	Folder cleared"
 	else:
 		os.makedirs(f)
-		print "	Folder created";
+		print "	Folder created"
 	os.chmod(f, 436)
 
 def ink(f):
@@ -57,79 +61,79 @@ def build(b):
 	config = cf()
 	browser = b
 	currDir = os.getcwd()
-	dbDir = pj(currDir, browser+"\\debug")
+	dbDir = pj([currDir, browser, "debug"])
 	print "     Build extension for "+browser
 	print tld
 	# Clear debug folder
 	int(dbDir)
 	# Create folders from config
 	for f in config['Create']:
-		os.makedirs(pj(dbDir, f));
+		os.makedirs(pj([dbDir, f]));
 	# Copy without replacement
 	for t in config['Copy']:
-		shutil.copy2(pj(currDir, 'Code\\'+t), pj(dbDir, t))
+		shutil.copy2(pj([currDir, 'Code', t]), pj([dbDir, t]))
 		print "	Copy "+t;
 	# Copy music
 	print "	Copy music"
 	for d in config['Music']:
 		ftc = d+'.'+config[browser]['Music']
-		shutil.copy2(pj(currDir, "Code\\music\\"+ftc), pj(dbDir, "music\\"+ftc));
+		shutil.copy2(pj([currDir, "Code", "music", ftc]), pj([dbDir, "music", ftc]))
 	# Copy whatsNew.js
-	shutil.copy2(pj(currDir, browser+'\\app\\js\\whatsNew.js'), pj(dbDir, 'js\\whatsNew.js'));
+	shutil.copy2(pj([currDir, browser, 'app', 'js', 'whatsNew.js']), pj([dbDir, 'js', 'whatsNew.js']))
 	# Copy background.html
 	if config[browser]['CopyBackgound'] == 'true':
-		shutil.copy2(pj(currDir, 'Code\\background.html'), pj(dbDir, 'background.html'));
+		shutil.copy2(pj([currDir, 'Code', 'background.html']), pj([dbDir, 'background.html']))
 	# Replace FUNCTIONS_FIRST_START
 	print "	Replace FUNCTIONS_FIRST_START on FunctionsFirstStart.js"
-	rp("{{FUNCTIONS_FIRST_START}}", rf(pj(currDir, browser+"\\app\\js\\FunctionsFirstStart.js")), pj(dbDir, "js\\functions.js"))
+	rp("{{FUNCTIONS_FIRST_START}}", rf(pj([currDir, browser, "app", "js", "FunctionsFirstStart.js"])), pj([dbDir, "js", "functions.js"]))
 	# Replace INTERVAL_STORAGE_CHANGE
 	print "	Replace INTERVAL_STORAGE_CHANGE on IntervalStorageChange.js"
-	rp("{{INTERVAL_STORAGE_CHANGE}}", rf(pj(currDir, browser+"\\app\\js\\IntervalStorageChange.js")), pj(dbDir, "js\\functions.js"))
+	rp("{{INTERVAL_STORAGE_CHANGE}}", rf(pj([currDir, browser, "app", "js", "IntervalStorageChange.js"])), pj([dbDir, "js", "functions.js"]))
 	# Replace APP_VERSION_CURRENT
 	print "	Replace APP_VERSION_CURRENT on "+config[browser]['Ver']
-	rp("{{APP_VERSION_CURRENT}}", "v."+config[browser]['Ver'], pj(dbDir, "js\\functions.js"))
+	rp("{{APP_VERSION_CURRENT}}", "v."+config[browser]['Ver'], pj([dbDir, "js", "functions.js"]))
 	# Replace NOTIFY_USER_FUNCTION
 	print "	Replace NOTIFY_USER_FUNCTION on notifications.js"
-	rp("{{NOTIFY_USER_FUNCTION}}", rf(pj(currDir, browser+"\\app\\js\\notifications.js")), pj(dbDir, "js\\functions.js"))
+	rp("{{NOTIFY_USER_FUNCTION}}", rf(pj([currDir, browser, "app", "js", "notifications.js"])), pj([dbDir, "js", "functions.js"]))
 	# Replace CSS_COMPILER
 	print "	Replace CSS_COMPILER on CSScompiler.js"
-	rp("{{CSS_COMPILER}}", rf(pj(currDir, browser+"\\app\\js\\CSScompiler.js")), pj(dbDir, "popup.js"))
+	rp("{{CSS_COMPILER}}", rf(pj([currDir, browser, "app", "js", "CSScompiler.js"])), pj([dbDir, "popup.js"]))
 	# Replace LINK_REVIEW
 	print "	Replace LINK_REVIEW on <link>"
-	rp("{{LINK_REVIEW}}", config[browser]['Review'], pj(dbDir, "popup.js"))
+	rp("{{LINK_REVIEW}}", config[browser]['Review'], pj([dbDir, "popup.js"]))
 	# Replace IF_BACKGROUND_BEGIN
 	print "	Replace IF_BACKGROUND_BEGIN on "+config[browser]['IfBackA']
-	rp("{{IF_BACKGROUND_BEGIN}}", config[browser]['IfBackA'], pj(dbDir, "js\\functions.js"))
+	rp("{{IF_BACKGROUND_BEGIN}}", config[browser]['IfBackA'], pj([dbDir, "js", "functions.js"]))
 	# Replace IF_BACKGROUND_END
 	print "	Replace IF_BACKGROUND_END on "+config[browser]['IfBackB']
-	rp("{{IF_BACKGROUND_END}}", config[browser]['IfBackB'], pj(dbDir, "js\\functions.js"))
+	rp("{{IF_BACKGROUND_END}}", config[browser]['IfBackB'], pj([dbDir, "js", "functions.js"]))
 	# Replace Platform
 	print "	Replace PLATFORM on "+config[browser]['Platform']
-	rp("{{PLATFORM}}", config[browser]['Platform'], pj(dbDir, "js\\functions.js"))
-	rp("{{PLATFORM}}", config[browser]['Platform'], pj(dbDir, "style.css"))
+	rp("{{PLATFORM}}", config[browser]['Platform'], pj([dbDir, "js", "functions.js"]))
+	rp("{{PLATFORM}}", config[browser]['Platform'], pj([dbDir, "style.css"]))
 	# Replace INSERT_SCRIPTS_HERE
 	print "	Replace INSERT_SCRIPTS_HERE on JS_scripts"
 	scrpts = ''
 	for y in config['Scripts']:
 		if y == "background.js":
 			if config[browser]['CopyBackgound'] != 'true':
-				scrpts += '<script src="'+y+'"></script>\n';
+				scrpts += '<script src="'+y+'"></script>\n'
 		else:
-			scrpts += '<script src="'+y+'"></script>\n';
-	rp("{{INSERT_SCRIPTS_HERE}}", str(scrpts), pj(dbDir, "popup.html"))
+			scrpts += '<script src="'+y+'"></script>\n'
+	rp("{{INSERT_SCRIPTS_HERE}}", str(scrpts), pj([dbDir, "popup.html"]))
 	# Replace INSERT_BACKGROUND_SCRIPTS
 	if config[browser]['CopyBackgound'] == 'true':
 		print "	Replace INSERT_BACKGROUND_SCRIPTS on JS_scripts"
 		scrBack = ''
 		for g in config['ScriptsBack']:
 			scrBack += '<script src="'+g+'"></script>\n'
-		rp('{{INSERT_BACKGROUND_SCRIPTS}}', str(scrBack), pj(dbDir, 'background.html'))
+		rp('{{INSERT_BACKGROUND_SCRIPTS}}', str(scrBack), pj([dbDir, 'background.html']))
 	# Replace BADGE_ONLINE_COUNT
 	print "	Replace BADGE_ONLINE_COUNT on BadgeOnlineCount.js"
-	rp("{{BADGE_ONLINE_COUNT}}", rf(pj(currDir, browser+"\\app\\js\\BadgeOnlineCount.js")), pj(dbDir, "js\\functions.js"))
+	rp("{{BADGE_ONLINE_COUNT}}", rf(pj([currDir, browser, "app", "js", "BadgeOnlineCount.js"])), pj([dbDir, "js", "functions.js"]))
 	# Inserting config file 'n' replace version
 	print "	Inserting "+config[browser]['Config']
-	shutil.copy2(pj(currDir, browser+'\\app\\'+config[browser]['Config']), pj(dbDir, config[browser]['Config']))
-	rp('{{APP_VERSION_CURRENT}}', config[browser]['Ver'], pj(dbDir, config[browser]['Config']))
+	shutil.copy2(pj([currDir, browser, 'app', config[browser]['Config']]), pj([dbDir, config[browser]['Config']]))
+	rp('{{APP_VERSION_CURRENT}}', config[browser]['Ver'], pj([dbDir, config[browser]['Config']]))
 	config[browser]['Build'] += 1
 	sc(config)
