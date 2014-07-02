dc45c3f70ffecc56957ed8d5833975ae7b5b576c
Ivan <hostingmcrozz@hotmail.com>
2014-07-02 14:07:54 +0600

    More proper way to get current version number

--- Chrome/app/js/FunctionsFirstStart.js
+++ Chrome/app/js/FunctionsFirstStart.js
@@ -4,16 +4,14 @@ if (window.location.pathname === '/background.html') {
     if (!localStorage.Status) localStorage.Status = '{"update":0,"online":0,"checked":0,"StopInterval":false}';
     if (!localStorage.FirstLaunch) localStorage.FirstLaunch='true';
     try { 
-        JSON.parse(localStorage.App_Version); 
-        $.getJSON('./manifest.json', function (d){
-            localJSON('App_Version', 'c', ['Got', 'v.'+d.version]);
-            localJSON('App_Version', 'c', ['Ver', 'v.'+d.version]);
-            if (local.App_Version.Ver !== 'v.'+d.version) {
-                notifyUser("Extension has been updated", "From "+local.App_Version.Ver+" to "+d.version, "ScriptUpdate", 'Upd'+Math.floor(Math.random(100)*100));
-                localStorage.App_Version_Update=true;
-                localStorage.App_Version_Try=0
-            }
-        });
+        var j = JSON.parse(localStorage.App_Version),
+            k = chrome.app.getDetails().version;
+        if (k !== j.Ver.substr(2)) {
+            notifyUser("Extension has been updated", "From "+j.Ver.substr(2)+" to "+k, "ScriptUpdate", 'Upd'+Math.floor(Math.random(100)*100));
+            localStorage.App_Version_Update=true;
+            localStorage.App_Version_Try=0;
+            localJSON('App_Version', ['Ver', k]);
+        }
     }
     catch(e) { localStorage.App_Version = '{"Ver": "{{APP_VERSION_CURRENT}}", "Got": "{{APP_VERSION_CURRENT}}"}'; localStorage.App_Version_Update=false; localStorage.App_Version_Try=0 }
     chrome.notifications.onButtonClicked.addListener(function(id){ window.open('http://www.twitch.tv/'+NameBuffer[id.match(/\d+/)[0]]) });
