b52399c460730e7158f1ce04de9f49956a81d421
Ivan <hostingmcrozz@hotmail.com>
2014-07-17 22:45:56 +0600

    A little code organization

--- Code/js/firstStart.js
+++ Code/js/firstStart.js
@@ -15,9 +15,9 @@
 */
 
 function reLogin() {
-	Animation('userChangePopup', ['bounceOut', true]);
-	Animation('userChangePopup2', ['fadeOut', true, 0.5]);
-	Animation('AppVersion', ['fadeIn', false]);
+	$('userChangePopup, userChangePopup2').fadeOut(500, function(){
+		$('#AppVersion').fadeIn(200)
+	});
 	if (opened) {
 		Animation('fndAbug', ['hideReportBtnA', true, 0.7]);
 		Animation('FoundAbugText', ['hideReportA', true, 0.7]);
@@ -37,23 +37,23 @@ function reLogin() {
 
 function lgin() {
 	if (localStorage.FirstLaunch === 'true'){
-		doc('FollowedChannelsOnline').innerHTML = "Greetings!";
+		$('#FollowedChannelsOnline').html("Greetings!");
 		doc('ChgUsr').disabled = true;
 		doc('LstFlwdChnls').disabled = true;
 		doc('Direct').disabled = true;
 		doc('Dashboard').disabled = true;
 
-		doc('insertContentHere').innerHTML = '<div class="fs1">'+
+		$('#insertContentHere').html('<div class="fs1">'+
 			'<a>Welcome!</a>'+
 			'<a>Please, choose how to login</a>'+
 			'<div id="TwitchAccount"><a>Using Twitct.TV account</a></div>'+
 			'<div id="TwitchName"><a>Using Twitct.TV username</a></div>'+
-			'</div>';
+			'</div>');
 			
 		// AUTH BY TWITCH ACCOUNT
 		$('#TwitchAccount, #TwitchAccount>a').on('click', function(){
-			doc('insertContentHere').innerHTML = '<iframe src="https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id=2p0gptvg3t1erx2h8fhbo9cwv8k5zq0&redirect_uri=http://twitchtvnotifier.host-ed.me/auth.php&scope=user_follows_edit+user_read" width="696" height="520" style="position:absolute;top:0;left:13" frameborder="0"></iframe>';
-			doc('FollowedChannelsOnline').innerHTML = "Sign in by Twicth Account";
+			$('#insertContentHere').html('<iframe src="https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id=2p0gptvg3t1erx2h8fhbo9cwv8k5zq0&redirect_uri=http://twitchtvnotifier.host-ed.me/auth.php&scope=user_follows_edit+user_read" width="696" height="520" style="position:absolute;top:0;left:13" frameborder="0"></iframe>');
+			$('#FollowedChannelsOnline').html("Sign in by Twicth Account");
 			t = 0;
 			$(window).on('message', function(e){
 				if (t !== 0) return false; t++;
@@ -70,10 +70,10 @@ function lgin() {
 							doc('LstFlwdChnls').disabled = false;
 							doc('Direct').disabled = false;
 							doc('Dashboard').disabled = false;
-					        localStorage.FirstLaunch = 'false';
-							doc('insertContentHere').innerHTML = null;
+					        localStorage.FirstLaunch = false;
+							$('#insertContentHere').html('');
 							localJSON('Status',['StopInterval',true]);
-							doc('FollowedChannelsOnline').innerHTML = "Please wait a moment";
+							$('#FollowedChannelsOnline').html("Please wait a moment");
 						} else { err({message:'Cannot get user name from response',stack:e}) }
 					}, error:function(e){err({message:'Tried to get username',stack:e})}});
 				} catch(e) { err(e); doc('FollowedChannelsOnline').innerHTML = "Error :(, please, restart extension"; }
@@ -88,7 +88,7 @@ function lgin() {
 			        localJSON('Status',['update',0]);
 			        localStorage.FirstLaunch = 'false';
 					localJSON('Status',['StopInterval',true]);
-					doc('FollowedChannelsOnline').innerHTML = "Please wait a moment";
+					$('#FollowedChannelsOnline').html("Please wait a moment");
 					doc('ChgUsr').disabled = false;
 					doc('LstFlwdChnls').disabled = false;
 					doc('Direct').disabled = false;
@@ -96,10 +96,10 @@ function lgin() {
 					doc('SetUpUserNameInp').onkeyup = function(){};
 					doc("SetUpUserName").onclick = function(){};
 					doc('insertContentHere').innerHTML = null;
-				} else { doc('FollowedChannelsOnline').innerHTML = 'Invalid name!' }
+				} else { $('#FollowedChannelsOnline').html('Invalid name!') }
 			}
-			doc('FollowedChannelsOnline').innerHTML = "Sign in by Twicth Name";
-			doc('insertContentHere').innerHTML = '<div class="Welcome" style="animated FadeIn">'+
+			$('#FollowedChannelsOnline').html("Sign in by Twicth Name");
+			$('#insertContentHere').html('<div class="Welcome" style="animated FadeIn">'+
 				'<p>Hello!</p>'+
 				'<p>Before you will use this app,</p>'+
 				'<p>could you say your Twitch.tv name?</p>'+
@@ -107,7 +107,7 @@ function lgin() {
 				'<p>Thanks for downloading this app!</p>'+
 				'<p>Hope this app will be useful for you</p>'+
 				'<button type="button" id="SetUpUserName" name="SetUpUserName" class="WelcomOK">OK</button>'+
-				'</div>';
+				'</div>');
 			doc('SetUpUserNameInp').focus();
 			doc("SetUpUserName").onclick = firstLaunchUserByName;
 			doc('SetUpUserNameInp').onkeyup = function (evt) { if (evt.keyCode == 13) firstLaunchUserByName() }
