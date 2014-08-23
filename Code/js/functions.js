b0d03cf549008f73de6633f0da6213c944a52697
Ivan <hostingmcrozz@hotmail.com>
2014-08-23 20:21:45 +0600

    Added LICENSE_HEADER

--- Code/js/functions.js
+++ Code/js/functions.js
@@ -1,18 +1,4 @@
-/*
-    Copyright 2014 Ivan 'MacRozz' Zarudny
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
+{{LICENSE_HEADER}}
 {{FUNCTIONS_FIRST_START}}
 if (localStorage.Ads === '') localStorage.Ads = '[]';
 function tm(j) { function g(s) { return s<10 ? '0'+s : s; } var d = new Date(); return '['+g(d.getHours())+':'+g(d.getMinutes())+':'+g(d.getSeconds())+']'+j; }
