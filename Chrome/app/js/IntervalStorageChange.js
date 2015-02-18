431f85a8acf5ce62701d28fb713b6f7c7971bf5e
Ivan <ivanzarudny@gmail.com>
2015-02-18 15:21:47 +0500

    Caught bug of chromium

--- Chrome/app/js/IntervalStorageChange.js
+++ Chrome/app/js/IntervalStorageChange.js
@@ -2,6 +2,8 @@ chrome.runtime.onMessage.addListener(function(msg) {
 	if (msg.type == "update")
 		loc();
 	else
+		window.parseMsg(msg);
 		// Background and popup pages will have different parseMsg
-		parseMsg(msg);
+		// BUG: function reference is working, but chromium thinks
+		// that is not...
 });
