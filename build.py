012cae6b530a3de9b29ab9d583dd6a93e201b659
Ivan <ivanzarudny@gmail.com>
2015-02-18 16:38:51 +0500

    Fixed year in license header in python files

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 """
-Copyright 2014 Ivan 'MacRozz' Zarudny
+Copyright 2014-2015 Ivan 'MacRozz' Zarudny
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
