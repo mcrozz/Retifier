ade055ba27496467ee49568983c9168b62f5fda1
Ivan <ivanzarudny@gmail.com>
2015-02-19 22:14:54 +0500

    Hotfix Twitch Notifier 1.4.1

--- Chrome/app/js/FunctionsFirstStart.js
+++ Chrome/app/js/FunctionsFirstStart.js
@@ -1,12 +1,15 @@
 if (window.location.pathname === '/background.html') {
     $.ajaxSetup ({cache:false,crossDomain:true});
     if (!localStorage.Config)
-        localStorage.Config = '{"User_Name":"Guest","token":"","Notifications":{"status":true,"online":true,"update":false,"sound_status":true,"sound":"DinDon","status":true,"follow":false},"Duration_of_stream":true,"Interval_of_Checking":3,"Format":"Grid"}';
+        localStorage.Config = '{"User_Name":"Guest","token":"","Notifications":{"status":true,"online":true,"offline":true,"update":false,"sound_status":true,"sound":"DinDon","status":true,"follow":false},"Duration_of_stream":true,"Interval_of_Checking":3,"Format":"Grid"}';
     if (!localStorage.Status)
         localStorage.Status = '{"update":7,"online":0,"checked":0}';
     if (!localStorage.FirstLaunch)
         localStorage.FirstLaunch='true';
-    try { 
+    if (!localStorage.FollowingList)
+        localStorage.FollowingList = '{}';
+    deb(localStorage.FollowingList);
+    try {
         var j = JSON.parse(localStorage.App_Version),
             k = chrome.runtime.getManifest().version;
         if (k !== j) {
@@ -15,6 +18,5 @@ if (window.location.pathname === '/background.html') {
             localStorage.App_Version_Try=0;
             localJSON('App_Version.Ver', k);
         }
-    }
-    catch(e) { localStorage.App_Version = '{"Ver": "'+chrome.runtime.getManifest().version+'", "Got": "'+chrome.runtime.getManifest().version+'"}'; localStorage.App_Version_Update=false; localStorage.App_Version_Try=0 }
+    } catch(e) { localStorage.App_Version = '{"Ver": "'+chrome.runtime.getManifest().version+'", "Got": "'+chrome.runtime.getManifest().version+'"}'; localStorage.App_Version_Update=false; localStorage.App_Version_Try=0 }
 }
