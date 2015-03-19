69276870b556f0cdb20fd7b46036752cb4cb6d8c
Ivan <ivanzarudny@gmail.com>
2015-03-19 21:12:56 +0500

    Moved from intervals to updates
    For example, if Storage. Status has been changed on the user side status
    bar will be updated

--- Chrome/app/js/parseMsgPopup.js
+++ Chrome/app/js/parseMsgPopup.js
@@ -2,4 +2,8 @@ window.parseMsg = function(msg) {
 	switch(msg.type) {
 		case "update": local.init(msg.data); break;
 	}
+	if (msg.type === 'update' && msg.data === 'FollowingList')
+		insert(msg.who);
+	else if (msg.type === 'update' && msg.data === 'Status')
+		updateStatus();
 };
--- Code/js/insertFunc.js
+++ Code/js/insertFunc.js
@@ -16,30 +16,62 @@ function snum() {
 	return g[local.Config.Format];
 }
 
-function InsertOnlineList() {
-	if (localStorage.FirstLaunch === 'true')
-		return false;
-	/*
-	* s - font size
-	* n - text
-	* w - width
-	*/
+var online = [];
+
+window.insert = function(name) {
+	deb(name);
+	// Return if already showing
+	if ($.inArray(name, online) !== -1)
+		return;
+
+	var obj = local.following.get(name);
+	// Streamer is offline or invalid input
+	if (!obj || !obj.Stream || obj.Name !== name)
+		return;
+
+	function stream(obj) {
+	  var name = obj.Name;
+	  var divs = {
+	    origin : $('#'+name),
+	    title  : $('#'+name+'>.inf>.title>a'),
+	    name   : $('#'+name+'>.inf>.streamer>a'),
+	    viewers: $('#'+name+'>.inf>.viewers>a'),
+	    game   : $('#'+name+'>.inf>.game>a'),
+	    ST     : $('#'+name+'>.tum>.ST'),
+	    GT1    : $('#'+name+'>.tum>.GT1'),
+	    GT2    : $('#'+name+'>.tum>.GT2')
+	  };
+	  var _this = this;
+	  $.each(divs, function(i,v) {
+	    _this[i] = {
+	      set: function(n) {
+	        v.html(n);
+	      },
+	      get: function() {
+	        return v.html();
+	      }
+	    };
+	  });
+	}
+
 	function offSet(t,w) {
+		// [t]ext, [w]hat(game or title)
 		var f = snum()[w];
 		var d = $('#textWidth').css({fontSize: f.f+'%', width: f.w+'%'}).html(t);
 		return (d.width()>(window.WIDTH*f.w));
 	}
-	/*
-	* a : {
-	*  str : streamer for urls
-	*  dsn : display streamer name
-	*  ttl : title
-	*  gme : game
-	*  viw : viewers count
-	*  pos : id
-	* }
-	*/
+
 	function insert(a) {
+		/*
+		* a : {
+		*  str : streamer for urls
+		*  dsn : display streamer name
+		*  ttl : title
+		*  gme : game
+		*  viw : viewers count
+		*  pos : id
+		* }
+		*/
 		var t = '', n = (a.gme !== 'Not Playing');
 		t += '<div class="content" id="'+a.pos+'">'
 			+'<div class="tum"><div class="loading" style="-webkit-transform:scale(0.35)"><div></div><div></div><div></div><div></div></div>'
@@ -60,6 +92,67 @@ function InsertOnlineList() {
 			+'<div class="duration"><a></a></div></div>';
 		_$('insertContentHere').innerHTML += t;
 	}
+
+	$('#insertContentHere').css('overflow', (local.Status.online <= 2)?'hidden':'auto');
+
+	var StreamTitle   = obj.Stream.Title,
+			StreamerName  = obj.Name,
+			ShortStrmName = obj.d_name,
+			StreamGame    = obj.Stream.Game,
+			eStreamGame   = encodeURI(StreamGame),
+			isGameThumb   = local.Games[eStreamGame],
+			StreamVievers = obj.Stream.Viewers,
+			TitleWidth    = false,
+			GameWidth     = false,
+			b             = '#'+StreamerName+'>',
+			dc;
+
+	if (typeof texts[StreamTitle] === 'undefined') {
+		texts[StreamTitle] = offSet(StreamTitle, 't');
+	} else { TitleWidth = texts[StreamTitle] }
+
+	if (typeof texts[StreamGame] === 'undefined') {
+		texts[StreamGame] = offSet(StreamGame, 'g');
+	} else { GameWidth = texts[StreamGame] }
+
+	if ($('#insertContentHere').html() === '<div class="NOO"><a>No one online right now :(</a></div>')
+		$('#insertContentHere').html('');
+
+	insert({
+		str: StreamerName,
+		dsn: ShortStrmName,
+		ttl: StreamTitle,
+		gme: StreamGame,
+		viw: StreamVievers,
+		pos: StreamerName
+	});
+	online.push(StreamerName);
+
+	TimersetToUpdate.push(ShortStrmName);
+
+	$(b+'.tum>.ST').css({
+		'background':'url(http://static-cdn.jtvnw.net/previews-ttv/live_user_'+StreamerName+'-320x200.jpg)',
+		'background-size':'contain',
+		'cursor':'pointer'
+	});
+	if (isGameThumb)
+		$(b+'.tum>.GT1').css({
+			'background':'url("http://static-cdn.jtvnw.net/ttv-boxart/'+eStreamGame+'-272x380.jpg")',
+			'background-size': 'contain',
+			'cursor':'pointer'
+		});
+	$(b+'.tum>.GT2').css({
+		'background':'url("./img/playing.png")',
+		'background-size': 'contain',
+		'cursor':'pointer'
+	});
+}
+
+function InsertOnlineList() {
+	if (localStorage.FirstLaunch === 'true')
+		return false;
+
+
 
 	$('#insertContentHere').css('overflow', (local.Status.online <= 2)?'hidden':'auto');
 
@@ -174,8 +267,7 @@ function InsertOnlineList() {
 	});
 }
 
-var pg = false, st = 1;
-setInterval(function(){
+window.updateStatus = function() {
 	/*
 			Status.update
 		0 :: Not updating, finished
@@ -203,22 +295,23 @@ setInterval(function(){
 		case 5: j.innerHTML = 'App have a problem with update'; break;
 		case 6: j.innerHTML = "Name doesn't set up yet!"; break;
 	}
-	pg = Upd!==0;
+	/*var pg = (Upd!==0);
 	if (pg) {
 		if (_$('CheckingProgress').hidden) {
 			$('#CheckingProgress').show();
 			_$('CheckingProgress').hidden=false;
 		}
 		_$('CheckingProgress').value = Math.floor( (100 / local.Following) * local.Status.checked);
-		if (st === 8) { Animation('refresh', ['spin', false, 0.8]); st = 1; }
-		else { st+=1 }
 	} else if (!_$('CheckingProgress').hidden) {
 		$('#CheckingProgress').hide();
 		_$('CheckingProgress').hidden=true;
-	}
-}, 100);
-
-window.onload = function(e){
-	setInterval(InsertOnlineList, 1000);
-	setTimeout(InsertOnlineList, 0);
+	}*/
 };
+
+$(function() {
+	updateStatus();
+	$.each(local.FollowingList, function(i,v) {
+		if (v.Stream)
+			insert(v.Name);
+	});
+});
