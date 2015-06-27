ab440d9d873007c515eba992bc0fb0212ca71abb
Ivan <ivanzarudny@gmail.com>
2015-06-27 18:10:27 +0500

    Experimenting with icon

--- Chrome/app/js/BadgeOnlineCount.js
+++ Chrome/app/js/BadgeOnlineCount.js
@@ -1,7 +1,10 @@
 function BadgeOnlineCount(count) {
-  chrome.browserAction.setBadgeText({ text: String(count) });
-  if (count !== 0)
-    chrome.browserAction.setBadgeBackgroundColor({color:"#593a94"});
-  else
-    chrome.browserAction.setBadgeBackgroundColor({color:"#c9c9c9"});
+	chrome.browserAction.setBadgeText({ text: String(count) });
+	if (count !== 0) {
+		chrome.browserAction.setBadgeBackgroundColor({color:"#593a94"});
+		chrome.browserAction.setIcon({path:"/img/icon_1.png"}, function(){});
+	} else {
+		chrome.browserAction.setBadgeBackgroundColor({color:"#c9c9c9"});
+		chrome.browserAction.setIcon({path:"/img/icon_0.png"}, function(){});
+	}
 }
