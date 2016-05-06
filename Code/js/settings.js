b23d42d91585f114a2cfa5b9525e4008ecbadd2c
Rozz <ivanzarudny@gmail.com>
2016-05-06 08:53:13 +0500

    Added ui.scale and notifications for settings fallback

--- Code/js/settings.js
+++ Code/js/settings.js
@@ -1,14 +1,22 @@
 window.settings = new storage('settings', {
 	fallback: {
-		ui: {},
+		ui: {
+			scale: null
+		},
 		checkInterval: 1000*60*5,
 		user: {
 			name: null,
 			id: null,
 			token: null
-		}
+		},
+		notifications: [
+			true, // went online
+			false,// changed title
+			true, // starts hosting
+			false // went offline
+		]
 	},
 	onchange: function(d) {
-		browser.send.async('settingsChanged', d);
+		browser.debug(d);
 	}
 });
