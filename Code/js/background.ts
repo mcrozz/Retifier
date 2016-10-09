2a7b010e3def069e2288952bf8c767758ba68378
Rozz <ivanzarudny@gmail.com>
2016-10-09 11:14:22 +0500

    Implemented Streamer class (function streamer)

---
+++ Code/js/background.ts
@@ -0,0 +1,34 @@
+﻿interface IStreamerPreviews {
+    stream: WindowBase64;
+    game: WindowBase64;
+    logo: WindowBase64;
+    profile: WindowBase64;
+}
+
+interface IStreamerData {
+    title: string;
+    game: string;
+    started: Date;
+    viewers: number;
+    previews: IStreamerPreviews;
+}
+
+class Streamer {
+    id: string;
+    name: string;
+    data: IStreamerData;
+    online: boolean;
+    lastUpdate: Date;
+    followingFrom: Date;
+
+    constructor(id: string, name: string, followingFrom: Date) {
+        this.id = id;
+        this.name = name;
+        this.followingFrom = followingFrom;
+    }
+
+    update = (): boolean => {
+        this.lastUpdate = new Date();
+        return true;
+    }
+}
