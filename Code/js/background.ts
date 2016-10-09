27c6182f284b192a99f307780243991539355fa9
Rozz <ivanzarudny@gmail.com>
2016-10-09 11:27:31 +0500

    Implemented Twitch class (function twitch)

--- Code/js/background.ts
+++ Code/js/background.ts
@@ -32,3 +32,36 @@ class Streamer {
         return true;
     }
 }
+
+class Twitch {
+    private streams = 'https://api.twitch.tv/kraken/streams/$0';
+    private followed = 'https://api.twitch.tv/kraken/streams/followed?limit=100&offset=0';
+    private preview = 'http://static-cdn.jtvnw.net/ttv-boxart/$0-272x380.jpg';
+    private hosting = 'http://api.twitch.tv/api/users/$0/followed/hosting?limit=50&offset=0';
+
+    getStatus = (user: string): JQueryXHR => {
+        user = encodeURIComponent(user);
+        const url = this.streams.replace('\$0', user);
+        return $.getJSON(url);
+    }
+
+    getFollowing = (user: string): JQueryXHR => {
+        user = encodeURIComponent(user);
+        const url = this.followed.replace('\$0', user);
+        return $.getJSON(url);
+    }
+
+    getGamePreview = (user: string): JQueryXHR => {
+        user = encodeURIComponent(user);
+        const url = this.preview.replace('\$0', user);
+        return $.get(url);
+    }
+
+    getHosting = (user: string): JQueryXHR => {
+        user = encodeURIComponent(user);
+        const url = this.hosting.replace('\$0', user);
+        return $.getJSON(url);
+    }
+}
+
+const twitch = new Twitch();
